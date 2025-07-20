0b313395ac8f37ddb51ad14a4d560949b5ef72bd
namit yadav <namityadav5255@gmail.com>
2025-07-20 20:54:50 +0530

    controller updated with bcrypt security

--- backend/controller/control.js
+++ backend/controller/control.js
@@ -1,25 +1,38 @@
 const mongoose  = require("mongoose")
 const model=require("../model/model")
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 
 async function dbconnect(url){
    const connect =await mongoose.connect(url)
    if(connect){
      console.log("✅ MongoDB connected");
    }
+   else{
+     console.log("not connected");
+   }
 }
 
 
-async function handlersingup_user(req,res) {
-    const{fullname,email,age,password}=req.body
+async function handlersingup_user(req,res){
+ const{fullname,email,age,password}=req.body
+       const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+    console.log(hash)    
    const usercreate= await model.create({
         fullname,
         email,
         age,
-        password
+        password:hash
     })
 res.send("done")
-console.log(req.body);
+if(!usercreate){
+  alert("something went wrong")
 }
+console.log(usercreate);   
+}
+
+
 
 async function handlersignup_ejs(req,res) {
     res.render("login")  
