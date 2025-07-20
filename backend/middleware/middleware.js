032a70b3bdd01e86aa22027d7611c4b2e2a5a969
namit yadav <namityadav5255@gmail.com>
2025-07-20 18:28:08 +0530

    middleware updated

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,7 +1,11 @@
+const router = require("../routes/routes");
+const cors=require("cors")
+const express=require("express")
+
 const middlewares = (app)=>{
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/signup",signup)
+app.use("/signup",router)
 }
 module.exports=middlewares
