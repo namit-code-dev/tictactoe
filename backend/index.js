6069a37b1d2c2d31f5ec429b7a26d8f5ccbb2e0f
namit yadav <namityadav5255@gmail.com>
2025-07-20 18:42:31 +0530

    server successflly connected with routes,contoller and db

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+const port=process.env.PORT || 8000;
+const {dbconnect}=require("./controller/control")
+const middlewares = require("./middleware/middleware")
+const dotenv =require("dotenv")
+dotenv.config()
+
+//database connect
+dbconnect(process.env.mongodb)
+
+//middleware
+middlewares(app)
+
+//view engine
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/views');
+
+//app listen
+app.listen(port,()=> console.log(`Server started on http://localhost:${port}`))
