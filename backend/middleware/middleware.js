3b57aa6deeab69809c53459adf9486693d2a88b9
namit yadav <namityadav5255@gmail.com>
2025-07-22 21:31:55 +0530

    new middlewares added

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,8 +1,10 @@
 const router = require("../routes/routes");
 const router_login=require("../routes/login")
+const htmlroute=require("../routes/htmlroute")
 const cookieParser = require("cookie-parser");
 const cors=require("cors")
 const express=require("express");
+const path = require("path");
 
 
 const middlewares = (app)=>{
@@ -12,5 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/signup",router)
 app.use(cookieParser())
 app.use("/login",router_login)
+app.use("/index.html",htmlroute)
+app.use(express.static(path.join(__dirname, "../../docs"), {
+  index: false
+}));
 }
 module.exports=middlewares
