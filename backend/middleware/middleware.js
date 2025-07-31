7a11bcad8078b1bb629f087b0bd3feadc4968a11
namit yadav <namityadav5255@gmail.com>
2025-07-31 21:50:37 +0530

    new middleware addedd

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,6 +1,7 @@
 const router = require("../routes/routes");
 const router_login=require("../routes/login")
 const htmlroute=require("../routes/htmlroute")
+const gameoption_routes=require("../routes/gameoption_routes")
 const cookieParser = require("cookie-parser");
 const cors=require("cors")
 const express=require("express");
@@ -17,6 +18,7 @@ app.use("/login",router_login)
 app.use("/namitgame",htmlroute)
 app.use("/img", express.static(path.join(__dirname, "../../docs/images_ejs")))
 app.use("/namitgame",handleindexhtml,express.static(path.join(__dirname, "../../docs")));
+app.use("/gameoption",gameoption_routes)
 
 }
 module.exports=middlewares
